3e264666bdc6b7b636b7cea8c9e9ccb770aab581
therealruvais <ruvais666@gmail.com>
2024-01-21 13:44:52 +0530

    new

--- frontend/src/axios-config.js
+++ frontend/src/axios-config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:1900/api",
+  baseURL: "https://pichub-server.onrender.com/api",
   withCredentials: true,
 });
 
